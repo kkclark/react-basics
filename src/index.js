9f2c73dbf2b96666a00181ec09dccab1dbe7102b
Kevin Clark <=>
2020-03-29 11:18:00 -0500

    get coords with geolocation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './seasonDisplay';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    // success callback
+    (position) => { console.log(position) },
+    // err callback
+    (err) => { console.log(err) }
+  );
   return(
     <SeasonDisplay />
   );
