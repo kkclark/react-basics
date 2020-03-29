7ce659055cb2775f53bb87fa16a6c2f78032cc7b
Kevin Clark <=>
2020-03-29 14:22:54 -0500

    initialize state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 //import SeasonDisplay from './seasonDisplay';
 
 class App extends React.Component {
+  // automatically called when instantiated before anything else
+  constructor(props) {
+    // must call super with props
+    super(props);
+
+    // calling super is what allows you to initialize state
+    this.state = { lat: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => { console.log(position) },
