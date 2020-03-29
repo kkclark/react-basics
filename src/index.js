595eec0e114e7359f5ef63e3680e51e77c7b9ed7
Kevin Clark <=>
2020-03-29 14:16:37 -0500

    translate a functional component into a class based component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './seasonDisplay';
+//import SeasonDisplay from './seasonDisplay';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    // success callback
-    (position) => { console.log(position) },
-    // err callback
-    (err) => { console.log(err) }
-  );
-  return(
-    <SeasonDisplay />
-  );
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => { console.log(position) },
+      (err) => { console.log(err) }
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
